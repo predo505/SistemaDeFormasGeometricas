174a677399c3d0f810b111fd109f2829acfcf633
predo505 <pedroesquecido1@gmail.com>
2025-04-29 01:09:40 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args) {
+        Circulo c = new Circulo(3.0);
+        Retangulo r = new Retangulo(3.0, 6.0);
+        Quadrado q = new Quadrado(4.0);
+
+        ArrayList<Forma> formas = new ArrayList<>();
+        formas.add(c);
+        formas.add(r);
+        formas.add(q);
+
+        for (Forma forma : formas) {
+            System.out.println("Forma: " + forma.nome());
+            System.out.println("Área: " + forma.calcularArea());
+            System.out.println("Perimetro: " + forma.calcularPerimetro());
+            System.out.println();
+        }
+    }
 }
