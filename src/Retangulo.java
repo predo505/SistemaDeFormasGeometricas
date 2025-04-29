5fc4ff9683ce955b0a593810c3835be850e52274
predo505 <pedroesquecido1@gmail.com>
2025-04-29 01:10:25 -0300

    Update Retangulo.java

--- src/Retangulo.java
+++ src/Retangulo.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+public class Retangulo extends Forma {
 
-public class Retangulo {
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    private double largura;
+    private double altura;
+
+    public Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public Retangulo() {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return largura * altura;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * (largura + altura);
+    }
+
+    @Override
+    public String nome() {
+        return "Retangulo";
+    }
 }
