fe57a950aca1a0ab8b9f0221b3726c78f42d4c83
predo505 <pedroesquecido1@gmail.com>
2025-04-29 01:09:57 -0300

    Update Quadrado.java

--- src/Quadrado.java
+++ src/Quadrado.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+public class Quadrado extends Forma {
+    public double getLado() {
+        return lado;
+    }
 
-public class Quadrado {
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    private double lado;
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.pow(lado, 2);
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 4 * lado;
+    }
+
+    @Override
+    public String nome() {
+        return "Quadrado";
+    }
 }
