73fba345b0fc0c6507009bc08e0acb9e75a7a050
predo505 <pedroesquecido1@gmail.com>
2025-04-29 01:09:09 -0300

    Update Circulo.java

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+public class Circulo extends Forma {
+    private double raio;
 
-public class Circulo {
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * raio * raio;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+
+    @Override
+    public String nome() {
+        return "Circulo";
+    }
 }
